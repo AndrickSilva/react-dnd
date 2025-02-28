a2f2d6495399ae975d2d2966d37f9a729fe5e822
pexeixv <gavineditz@gmail.com>
2025-02-28 16:13:21 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { Github } from "lucide-react";
+import "./App.css";
+import { KanbanBoard } from "./components/KanbanBoard";
+import { ThemeToggle } from "./components/ThemeToggle";
+import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
+
+const FooterLink = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Button
+      variant="link"
+      asChild
+      className="text-xl font-semibold tracking-tight scroll-m-20"
+    >
+      {children}
+    </Button>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="flex flex-col min-h-screen">
+          <main className="flex flex-col h-full gap-6 mx-4">
+            <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
+              Drag and Drop Kanban Board
+            </h1>
+            <KanbanBoard />
+          </main>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
